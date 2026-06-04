8d310fd78a77f190e841b6172f1f93ac87d58cd8
Rahamat-prog <rahamatshaikh216@gmail.com>
2026-06-04 17:18:20 +0530

    basic redux toolkit setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,20 @@ import './index.css'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import App from './App.jsx'
+import store from './Redux/store'
 
 
 
 createRoot(document.getElementById('root')).render(
     // BrowserRouter karne se react router dom ki jo bhi capability h oh app active ho jaye ha 
-    <BrowserRouter>
+   <Provider store={store}>
+     <BrowserRouter>
         <App />
         <Toaster />
-
     </BrowserRouter>
+   </Provider>
 
 )
