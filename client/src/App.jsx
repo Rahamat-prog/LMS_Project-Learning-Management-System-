6432e3e0d79aec1b10aaf902cd385199a6688e6c
Rahamat-prog <rahamatshaikh216@gmail.com>
2026-06-09 02:36:05 +0530

    feat: footer section defined and axios intance confing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
-
+import Footer from './components/Footer';
 
 
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>LMS Project</h1>
+      <Footer />
     </>
   )
 }
---
+++ client/src/Helpers/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/v1";
+
+// to sent req with the all credential like cookie and header we need to set withCredentials to true in the axios instance
+const axiosInstance = axios.create();
+
+axiosInstance.defaults.baseURL= BASE_URL;
+axiosInstance.defaults.withCredentials = true;
+
+export default axiosInstance;
