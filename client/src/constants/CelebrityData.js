7b5438ae63824e9d0688ef59a8ab2e7b56ae9528
Rahamat-prog <rahamatshaikh216@gmail.com>
2026-06-15 21:18:30 +0530

    refactor: cleaned up our About us page

---
+++ client/src/constants/CelebrityData.js
@@ -0,0 +1,38 @@
+import apj from '../assets/Images/apj.png'
+import billGates from '../assets/Images/billGates.png'
+import einstein from '../assets/Images/einstein.png'
+import nelsonMandela from '../assets/Images/nelsonMandela.png'
+import steveJob from '../assets/Images/steveJobs.png'
+  
+  export const celebrities = [ 
+        {
+            title: "APJ Abdul Kalam",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempore",
+            image: apj,
+            slideNumber: 1
+        },
+        {
+            title: "Bill Gates",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempore",
+            image: billGates,
+            slideNumber: 2
+        },
+        {
+            title: "einstein",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempore",
+            image: einstein,
+            slideNumber: 3
+        },
+        {
+            title: "nelsonMandela",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempore",
+            image: nelsonMandela,
+            slideNumber: 4
+        },
+        {
+            title: "steveJob",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tempore",
+            image: steveJob,
+            slideNumber: 5
+        },
+    ]
