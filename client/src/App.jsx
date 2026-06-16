91c8f7f343f86f98987af95a79ce0069e77e0bfb
Rahamat-prog <rahamatshaikh216@gmail.com>
2026-06-16 19:13:22 +0530

    feat : NotFount page defined

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import AboutUs from './Pages/AboutUs';
 import HomePage from './Pages/HomePage';
 import {Route, Routes} from 'react-router-dom'
-
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <HomePage /> }></Route>
         <Route path='/about' element={ <AboutUs /> }></Route>
+        <Route path='*' element={<NotFound />} > </Route>
       </Routes>
     </>
   )
